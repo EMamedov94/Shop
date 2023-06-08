ae3da233ce508c39ad7115216538d6189866f08e
el <mamedov094@bk.ru>
2023-06-09 02:13:55 +0300

    add get photo worked

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -16,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @RestController
@@ -24,9 +27,6 @@ import java.util.UUID;
 @CrossOrigin(origins = "https://localhost:4200/")
 public class ProductController {
     private final ProductService productService;
-
-//    @Value("${upload.path}")
-//    private String uploadPath;
 
     // Add new product to shop
     @PostMapping("/addNewProduct")
@@ -37,16 +37,16 @@ public class ProductController {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         if (photoUrl != null) {
-            File uploadDir = new File("src/main/resources/images");
+            File uploadDir = new File("D:\\java\\Shop-front\\src\\assets\\images\\products"
+                    + "\\" + user.getUsername());
             if (!uploadDir.exists()) {
                 uploadDir.mkdir();
             }
             String uuidFile = UUID.randomUUID().toString();
             String resultFileName = uuidFile + "." + photoUrl.getOriginalFilename();
+            photoUrl.transferTo(new File(uploadDir + "\\" + resultFileName));
 
-            photoUrl.transferTo(new File(resultFileName));
-
-            product.setPhotoUrl(resultFileName);
+            product.setPhotoUrl("/assets/images/products/" + user.getUsername() + "/" + resultFileName);
         }
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
@@ -55,45 +55,6 @@ public class ProductController {
                 .headers(headers)
                 .body(productService.addNewProductToShop(product, user.getUsername()));
     }
-
-
-//    @PostMapping("/addNewProduct")
-//    public ResponseEntity<Product> addNewProduct(@AuthenticationPrincipal UserDetails user,
-//                                                 @RequestParam(name = "name") String name,
-//                                                 @RequestParam(name = "description") String description,
-//                                                 @RequestParam(name = "price") Double price,
-//                                                 @RequestParam(name = "qty") Integer qty,
-//                                                 @RequestParam(name = "photoUrl") MultipartFile photoUrl) throws IOException {
-//        Product prodd = Product.builder()
-//                .name(name)
-//                .description(description)
-//                .price(price)
-//                .qty(qty)
-//                .build();
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        if (photoUrl != null) {
-//            File uploadDir = new File("src/main/resources/images");
-//            if (!uploadDir.exists()) {
-//                uploadDir.mkdir();
-//            }
-//            String uuidFile = UUID.randomUUID().toString();
-//            String resultFileName = uuidFile + "." + photoUrl.getOriginalFilename();
-//
-//            photoUrl.transferTo(new File(resultFileName));
-//
-//            prodd.setPhotoUrl(resultFileName);
-//        }
-//        if (user == null) {
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        }
-//        return ResponseEntity.ok()
-//                .headers(headers)
-//                .body(productService.addNewProductToShop(prodd, user.getUsername()));
-//    }
 
     @DeleteMapping("/deleteProductFromShop/{id}")
     public ResponseEntity<String> deleteProductFromShop(@PathVariable Long id) {
